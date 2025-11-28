81134f7ea2c338fca264d436a866943d7a0a1106
fajri <fajri42vandi@gmail.com>
2025-11-28 11:32:50 +0700

    refactor: remove trycatch

--- src/controllers/todo.ctrl.js
+++ src/controllers/todo.ctrl.js
@@ -2,46 +2,26 @@ import * as TodoService from '../services/todo.service.js';
 import response from '../utils/response.js';
 
 export const getTodoList = async (req, res, next) => {
-    try {
-        const data = await TodoService.getAllData();
-        response(res, 200, 'Success', data);
-    } catch (err) {
-        next(err);
-    }
+    const data = await TodoService.getAllData();
+    response(res, 200, 'Success', data);
 };
 
 export const getDetailTodo = async (req, res, next) => {
-    try {
-        const data = await TodoService.getOneData(req.params.id);
-        response(res, 200, 'Success', data);
-    } catch (err) {
-        next(err);
-    }
+    const data = await TodoService.getOneData(req.params.id);
+    response(res, 200, 'Success', data);
 };
 
 export const createTodo = async (req, res, next) => {
-    try {
-        const data = await TodoService.createData(req.body);
-        response(res, 200, 'Success', data);
-    } catch (err) {
-        next(err);
-    }
+    const data = await TodoService.createData(req.body);
+    response(res, 200, 'Success', data);
 };
 
 export const updateTodo = async (req, res, next) => {
-    try {
-        const data = await TodoService.updateData(req.params.id, req.body);
-        response(res, 200, 'Success', data);
-    } catch (error) {
-        next(err);
-    }
+    const data = await TodoService.updateData(req.params.id, req.body);
+    response(res, 200, 'Success', data);
 };
 
 export const deleteTodo = async (req, res, next) => {
-    try {
-        await TodoService.deleteData(req.params.id);
-        response(res, 200, 'Success', req.params.id);
-    } catch (err) {
-        next(err);
-    }
+    await TodoService.deleteData(req.params.id);
+    response(res, 200, 'Success', req.params.id);
 };
