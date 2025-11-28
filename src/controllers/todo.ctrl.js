8847489b25f31a627d194aab9457d15b5a63779d
fajri <fajri42vandi@gmail.com>
2025-11-28 15:17:22 +0700

    fix: change eslint configure

--- src/controllers/todo.ctrl.js
+++ src/controllers/todo.ctrl.js
@@ -1,27 +1,27 @@
 import * as TodoService from '../services/todo.service.js';
 import response from '../utils/response.js';
 
-export const getTodoList = async (req, res, next) => {
+export const getTodoList = async (req, res) => {
     const data = await TodoService.getAllData();
     response(res, 200, 'Success', data);
 };
 
-export const getDetailTodo = async (req, res, next) => {
+export const getDetailTodo = async (req, res) => {
     const data = await TodoService.getOneData(req.params.id);
     response(res, 200, 'Success', data);
 };
 
-export const createTodo = async (req, res, next) => {
+export const createTodo = async (req, res) => {
     const data = await TodoService.createData(req.body);
     response(res, 200, 'Success', data);
 };
 
-export const updateTodo = async (req, res, next) => {
+export const updateTodo = async (req, res) => {
     const data = await TodoService.updateData(req.params.id, req.body);
     response(res, 200, 'Success', data);
 };
 
-export const deleteTodo = async (req, res, next) => {
+export const deleteTodo = async (req, res) => {
     await TodoService.deleteData(req.params.id);
     response(res, 200, 'Success', req.params.id);
 };
